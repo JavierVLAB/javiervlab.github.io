4700d7f12d9c801e86d1bc8461be60e6ab2d9114
JaviVLAB <javier.zen@gmail.com>
2024-10-14 15:58:48 +0200

    update 14 oct

--- src/pages/GalleryPost.js
+++ src/pages/GalleryPost.js
@@ -6,12 +6,34 @@ const GalleryPost = ({project}) => (
 
 	<div className="col-xs-12 col-sm-6 col-md-4 project_square">
 		<div className="project_square_background">
-			{/*<Link to={`project${project.id}`}>*/}
-				<img alt={`${project.title}`} src={`${project.imageUrl}`} className="project_square_images"/>
-				<p className="project_text">{project.title}
-				<br/>
-  			<span className="project-category">{project.category}</span></p>
-			{/*</Link>*/}
+		{project.link ? (
+			<a href={project.link} target="_blank" rel="noreferrer">
+				<img
+				alt={project.title}
+				src={project.imageUrl}
+				className="project_square_images"
+				/>
+				<p className="project_text">
+				{project.title}
+				<br />
+				<span className="project-category">{project.category}</span>
+				</p>
+			</a>
+			) : (
+			<div className="project_no_link">
+				<img
+				alt={project.title}
+				src={project.imageUrl}
+				className="project_square_images"
+				/>
+				<p className="project_text">
+				{project.title}
+				<br />
+				<span className="project-category">{project.category}</span>
+				</p>
+			</div>
+			)}
+
 			<Route path={`project${project.id}`} component={Project} />
 		</div>
 	</div>
